23c5fea2fea6281ba34def9831e0c55f5bd9d0da
Heli Huhtilainen <heli.huhtilainen@helsinki.fi>
2018-11-06 16:19:01 +0200

    viikko2, tehtävä 2

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -10,24 +10,18 @@ public class Varasto {
     public Varasto(double tilavuus) {  // tilavuus on annettava
         if (tilavuus > 0.0) {
             this.tilavuus = tilavuus;
-        } else // virheellinen, nollataan
-        {
+            // virheellinen, nollataan
+        } else {
             this.tilavuus = 0.0;  // => käyttökelvoton varasto
         }
         saldo = 0;     // oletus: varasto on tyhjä
     }
 
     public Varasto(double tilavuus, double alkuSaldo) { // kuormitetaan
-        if (tilavuus > 0.0) {
-            this.tilavuus = tilavuus;
-        } else // virheellinen, nollataan
-        {
-            this.tilavuus = 0.0;  // => käyttökelvoton varasto
-        }
-        if (alkuSaldo < 0.0) {
-            this.saldo = 0.0;
-        } else if (alkuSaldo <= this.tilavuus) // mahtuu
-        {
+        
+        this.tilavuus = Math.max(0, tilavuus);
+        
+        if (alkuSaldo <= this.tilavuus && alkuSaldo >= 0) {
             this.saldo = alkuSaldo;
         } else {
             this.saldo = this.tilavuus;  // täyteen ja ylimäärä hukkaan!
@@ -49,12 +43,12 @@ public class Varasto {
 
     // --- asettavat aksessorit eli setterit: ---
     public void lisaaVarastoon(double maara) {
-        if (maara < 0) // virhetilanteessa voidaan tehdä 
-        {
+        // virhetilanteessa voidaan tehdä 
+        if (maara < 0) {
             return;       // tällainen pikapoistuminenkin!
         }
-        if (maara <= paljonkoMahtuu()) // omia aksessoreita voi kutsua
-        {
+        // omia aksessoreita voi kutsua
+        if (maara <= paljonkoMahtuu()) {
             saldo = saldo + maara;          // ihan suoraan sellaisinaan
         } else {
             saldo = tilavuus;  // täyteen ja ylimäärä hukkaan!
@@ -62,8 +56,8 @@ public class Varasto {
     }
 
     public double otaVarastosta(double maara) {
-        if (maara < 0) // virhetilanteessa voidaan tehdä 
-        {
+        // virhetilanteessa voidaan tehdä 
+        if (maara < 0) {
             return 0.0;   // tällainen pikapoistuminenkin!
         }
         if (maara > saldo) {          // annetaan mitä voidaan
